bf16cda91395a9a1822f777e9116c327943fc03c
Greg Niederschulte <gtn@u.northwestern.edu>
2017-04-07 12:45:09 -0500

    Correct Code (verified it works) and the remainder of HW1 assignment

--- HW1/HW1.X/main.c
+++ HW1/HW1.X/main.c
@@ -2,39 +2,39 @@
 #include<sys/attribs.h>  // __ISR macro
 
 // DEVCFG0
-#pragma config DEBUG = 11 // no debugging
-#pragma config JTAGEN = 0 // no jtag
-#pragma config ICESEL = 11 // use PGED1 and PGEC1
-#pragma config PWP = 111111111 // no write protect
-#pragma config BWP = 1 // no boot write protect
-#pragma config CP = 1 // no code protect
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
 
 // DEVCFG1
-#pragma config FNOSC = 011 // use primary oscillator with pll
-#pragma config FSOSCEN = 0 // turn off secondary oscillator
-#pragma config IESO = 0 // no switching clocks
-#pragma config POSCMOD = 10 // high speed crystal mode
-#pragma config OSCIOFNC = 1 // disable secondary osc
-#pragma config FPBDIV = 00 // divide sysclk freq by 1 for peripheral bus clock
-#pragma config FCKSM = 11 // do not enable clock switch
-#pragma config WDTPS = 00000 // use slowest wdt
-#pragma config WINDIS = 1 // wdt no window mode
-#pragma config FWDTEN = 0 // wdt disabled
-#pragma config FWDTWINSZ = 11 // wdt window at 25%
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable secondary osc
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576 // use slowest wdt
+#pragma config WINDIS = OFF // wdt no window mode
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
-#pragma config FPLLIDIV = 001 // divide input clock to be in range 4-5MHz
-#pragma config FPLLMUL = 111 // multiply clock after FPLLIDIV
-#pragma config FPLLODIV = 001 // divide clock after FPLLMUL to get 48MHz
-#pragma config UPLLIDIV = 001 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
-#pragma config UPLLEN = 0 // USB clock on
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
 
 // DEVCFG3
-#pragma config USERID = 0001000100010001 // some 16bit userid, doesn't matter what
-#pragma config PMDL1WAY = 0 // allow multiple reconfigurations
-#pragma config IOL1WAY = 0 // allow multiple reconfigurations
-#pragma config FUSBIDIO = 1 // USB pins controlled by USB module
-#pragma config FVBUSONIO = 1 // USB BUSON controlled by USB module
+#pragma config USERID = 0x0 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
 
 int main() {
@@ -67,13 +67,13 @@ int main() {
         LATAbits.LATA4 = 1; //RA4 high initially means the USER LED starts on
         
         _CP0_SET_COUNT(0); //restart timer
-        while(_CP0_SET_COUNT() < 12000)
+        while(_CP0_GET_COUNT() < 12000)
             {;} //wait 12000 ticks = 0.5 ms at 24 MHz
         
         LATAbits.LATA4 = 0; //RA4 now low turns the USER LED off
         
         _CP0_SET_COUNT(0); //restart timer
-        while(_CP0_SET_COUNT() < 12000)
+        while(_CP0_GET_COUNT() < 12000)
             {;} //wait 12000 ticks = 0.5 ms at 24 MHz
         
         while(PORTBbits.RB4 == 0) //While the user button is pushed
